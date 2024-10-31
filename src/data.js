33c56588c5c8f2fdd272ec79c0c25231ed16cada
Matheus Rodrigues <MatheusUnkamed@outlook.com>
2024-10-31 17:44:54 -0300

    name fix

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 export const conversations = [
-    { id: '1', name: 'João', img: 'https://img.freepik.com/premium-photo/happy-man-ai-generated-portrait-user-profile_1119669-1.jpg', msg: 'Boa Tarde' },
-    { id: '2', name: 'Maria', img: 'https://imgv3.fotor.com/images/gallery/AI-3D-Female-Profile-Picture.jpg', msg: 'Sim'},
-    { id: '3', name: 'Carlos', img: 'https://imgv3.fotor.com/images/gallery/Realistic-Male-Profile-Picture.jpg', msg: 'Acho que não' },
-    { id: '4', name: 'Pedro', img: 'https://img.freepik.com/premium-photo/happy-man-ai-generated-portrait-user-profile_1119669-1.jpg', msg: 'Ok' },
-    { id: '5', name: 'Felipe', img: 'https://imgv3.fotor.com/images/gallery/AI-3D-Female-Profile-Picture.jpg', msg: 'Entendi'},
-    { id: '6', name: 'Cesar', img: 'https://imgv3.fotor.com/images/gallery/Realistic-Male-Profile-Picture.jpg', msg: 'Pode ser' }
+    { id: '1', name: 'João', img: 'https://img.freepik.com/premium-photo/happy-man-ai-generated-portrait-user-profile_1119669-1.jpg', msg: 'Boa Tarde', isMine: true },
+    { id: '2', name: 'Maria', img: 'https://imgv3.fotor.com/images/gallery/AI-3D-Female-Profile-Picture.jpg', msg: 'Sim', isMine: false },
+    { id: '3', name: 'Carlos', img: 'https://imgv3.fotor.com/images/gallery/Realistic-Male-Profile-Picture.jpg', msg: 'Acho que não', isMine: true},
+    { id: '4', name: 'Pedro', img: 'https://img.freepik.com/premium-photo/happy-man-ai-generated-portrait-user-profile_1119669-1.jpg', msg: 'Ok', isMine: true },
+    { id: '5', name: 'Carla', img: 'https://imgv3.fotor.com/images/gallery/AI-3D-Female-Profile-Picture.jpg', msg: 'Entendi', isMine: false },
+    { id: '6', name: 'Cesar', img: 'https://imgv3.fotor.com/images/gallery/Realistic-Male-Profile-Picture.jpg', msg: 'Pode ser', isMine: false }
 ];
